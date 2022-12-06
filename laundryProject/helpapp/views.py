e728dd394ae18583e783d8d9ee56a47a9b6d49de
akajaguma <kasapontasu@gmail.com>
2022-12-06 15:01:37 +0900

    profileを追加しました
    userのイメージが表示されないけどほぞんしてないだけだからね

--- laundryProject/helpapp/views.py
+++ laundryProject/helpapp/views.py
@@ -299,6 +299,7 @@ def user(request):
         return render(request, 'user/index.html', context)
     else :
         return redirect('/accounts/login/')
+
 def timeline(request):
     if request.user.is_authenticated :
         user = request.user
@@ -435,6 +436,7 @@ def profile_add(request):
             'cabinet_form': CabinetForm(),
         }
     return render(request, 'cabinet/add.html', context)
+
 
 import torch
 from django.shortcuts import render
